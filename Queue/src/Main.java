7454bee1cf9f7d8aac3878330ca8003b4d028e4e
LiangSir-67 <1732178815@qq.com>
2021-04-25 14:41:51 +0800

    队列

---
+++ Queue/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * @Author: 梁歪歪 <1732178815@qq.com>
+ * @Description: blog <liangyy.cn>
+ * @Create 2021-04-23-10:09
+ */
+public class Main {
+    public static void main(String[] args) {
+        ArrayQueue<Integer> queue = new ArrayQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if (i % 3 == 2){
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
+}
---
+++ Queue/src/Queue.java
@@ -0,0 +1,12 @@
+/**
+ * @Author: 梁歪歪 <1732178815@qq.com>
+ * @Description: blog <liangyy.cn>
+ * @Create 2021-04-25-14:23
+ */
+public interface Queue<E> {
+    int getSize();
+    boolean isEmpty();
+    void enqueue(E e);
+    E dequeue();
+    E getFront();
+}
