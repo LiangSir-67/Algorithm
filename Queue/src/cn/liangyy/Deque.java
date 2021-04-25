64755c6dffef88b7b9c12c8b8abc05e4b7bc08bb
LiangSir-67 <1732178815@qq.com>
2021-04-25 15:58:05 +0800

    双端队列

--- Queue/src/cn/liangyy/Deque.java
+++ Queue/src/cn/liangyy/Deque.java
@@ -13,44 +13,44 @@ public class Deque<E> {
     private int front, tail;
     private int size; // 方便起见，我们的 Deque 实现，将使用 size 记录 deque 中存储的元素数量
 
-    public Deque(int capacity){
-        data = (E[])new Object[capacity]; // 由于使用 size，我们的 Deque 实现不浪费空间
+    public Deque(int capacity) {
+        data = (E[]) new Object[capacity]; // 由于使用 size，我们的 Deque 实现不浪费空间
         front = 0;
         tail = 0;
         size = 0;
     }
 
-    public Deque(){
+    public Deque() {
         this(10);
     }
 
-    public int getCapacity(){
+    public int getCapacity() {
         return data.length;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
     // addLast 的逻辑和我们之前实现的队列中的 enqueue 的逻辑是一样的
-    public void addLast(E e){
+    public void addLast(E e) {
 
-        if(size == getCapacity())
+        if (size == getCapacity())
             resize(getCapacity() * 2);
 
         data[tail] = e;
         tail = (tail + 1) % data.length;
-        size ++;
+        size++;
     }
 
     // addFront 是新的方法，请大家注意
-    public void addFront(E e){
+    public void addFront(E e) {
 
-        if(size == getCapacity())
+        if (size == getCapacity())
             resize(getCapacity() * 2);
 
         // 我们首先需要确定添加新元素的索引位置
@@ -58,49 +58,49 @@ public class Deque<E> {
         // 但是要注意，如果 front == 0，新的位置是 data.length - 1 的位置
         front = front == 0 ? data.length - 1 : front - 1;
         data[front] = e;
-        size ++;
+        size++;
     }
 
     // removeFront 的逻辑和我们之前实现的队列中的 dequeue 的逻辑是一样的
-    public E removeFront(){
+    public E removeFront() {
 
-        if(isEmpty())
+        if (isEmpty())
             throw new IllegalArgumentException("Cannot dequeue from an empty queue.");
 
         E ret = data[front];
         data[front] = null;
         front = (front + 1) % data.length;
-        size --;
-        if(getSize() == getCapacity() / 4 && getCapacity() / 2 != 0)
+        size--;
+        if (getSize() == getCapacity() / 4 && getCapacity() / 2 != 0)
             resize(getCapacity() / 2);
         return ret;
     }
 
     // removeLast 是新的方法，请大家注意
-    public E removeLast(){
+    public E removeLast() {
 
-        if(isEmpty())
+        if (isEmpty())
             throw new IllegalArgumentException("Cannot dequeue from an empty queue.");
 
         // 计算删除掉队尾元素以后，新的 tail 位置
         tail = tail == 0 ? data.length - 1 : tail - 1;
         E ret = data[tail];
         data[tail] = null;
-        size --;
-        if(getSize() == getCapacity() / 4 && getCapacity() / 2 != 0)
+        size--;
+        if (getSize() == getCapacity() / 4 && getCapacity() / 2 != 0)
             resize(getCapacity() / 2);
         return ret;
     }
 
-    public E getFront(){
-        if(isEmpty())
+    public E getFront() {
+        if (isEmpty())
             throw new IllegalArgumentException("cn.liangyy.Queue is empty.");
         return data[front];
     }
 
     // 因为是双端队列，我们也有一个 getLast 的方法，来获取队尾元素的值
-    public E getLast(){
-        if(isEmpty())
+    public E getLast() {
+        if (isEmpty())
             throw new IllegalArgumentException("cn.liangyy.Queue is empty.");
 
         // 因为 tail 指向的是队尾元素的下一个位置，我们需要计算一下真正队尾元素的索引
@@ -108,10 +108,10 @@ public class Deque<E> {
         return data[index];
     }
 
-    private void resize(int newCapacity){
+    private void resize(int newCapacity) {
 
-        E[] newData = (E[])new Object[newCapacity];
-        for(int i = 0 ; i < size ; i ++)
+        E[] newData = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++)
             newData[i] = data[(i + front) % data.length];
 
         data = newData;
@@ -120,34 +120,34 @@ public class Deque<E> {
     }
 
     @Override
-    public String toString(){
+    public String toString() {
 
         StringBuilder res = new StringBuilder();
         res.append(String.format("cn.liangyy.Queue: size = %d , capacity = %d", getSize(), getCapacity()));
-                res.append("front [");
-        for(int i = 0 ; i < size ; i ++){
+        res.append("front [");
+        for (int i = 0; i < size; i++) {
             res.append(data[(i + front) % data.length]);
-            if(i != size - 1)
+            if (i != size - 1)
                 res.append(", ");
         }
         res.append("] tail");
         return res.toString();
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         // 在下面的双端队列的测试中，偶数从队尾加入；奇数从队首加入
         Deque<Integer> dq = new Deque<>();
-        for(int i = 0 ; i < 16 ; i ++){
-            if(i % 2 == 0) dq.addLast(i);
+        for (int i = 0; i < 16; i++) {
+            if (i % 2 == 0) dq.addLast(i);
             else dq.addFront(i);
             System.out.println(dq);
         }
 
         // 之后，我们依次从队首和队尾轮流删除元素
         System.out.println();
-        for(int i = 0; !dq.isEmpty(); i ++){
-            if(i % 2 == 0) dq.removeFront();
+        for (int i = 0; !dq.isEmpty(); i++) {
+            if (i % 2 == 0) dq.removeFront();
             else dq.removeLast();
             System.out.println(dq);
         }
