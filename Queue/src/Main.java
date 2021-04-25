1d9dc0b34ec0aab612006f9cd3e1429ad393b22f
LiangSir-67 <1732178815@qq.com>
2021-04-25 15:36:55 +0800

    循环队列

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * @Author: 梁歪歪 <1732178815@qq.com>
  * @Description: blog <liangyy.cn>
@@ -5,15 +7,41 @@
  */
 public class Main {
     public static void main(String[] args) {
-        ArrayQueue<Integer> queue = new ArrayQueue<>();
-        for (int i = 0; i < 10; i++) {
-            queue.enqueue(i);
-            System.out.println(queue);
 
-            if (i % 3 == 2){
-                queue.dequeue();
-                System.out.println(queue);
-            }
+        int opCount = 100000;
+
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue, time: "+ time1 + "s");
+
+        System.out.println("------------------------------");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue, time: "+ time2 + "s");
+    }
+
+    /**
+     * 测试使用q 运行opCount个enqueue和dequeue操作所需要的时间，单位：秒
+     *
+     * @param q
+     * @param opCount
+     * @return
+     */
+    private static double testQueue(Queue<Integer> q, int opCount){
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
         }
+
+        for (int i = 0; i < opCount; i++) {
+            q.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
     }
 }
