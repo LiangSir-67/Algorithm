595f9d1c44119cdd0fadb0f3f34cbe29507d3cea
LiangSir-67 <1732178815@qq.com>
2021-04-26 11:03:03 +0800

    用链表实现栈

---
+++ Stack/src/cn/liangyy/LinkedListStack.java
@@ -0,0 +1,60 @@
+package cn.liangyy;
+
+/**
+ * 用链表实现栈
+ *
+ * @Author: 梁歪歪 <1732178815@qq.com>
+ * @Description: blog <liangyy.cn>
+ * @Create 2021-04-26-10:44
+ */
+public class LinkedListStack<E> implements Stack<E> {
+
+    private LinkedList<E> list;
+
+    public LinkedListStack(){
+        list = new LinkedList<>();
+    }
+
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public void push(E e) {
+        list.addFirst(e);
+    }
+
+    @Override
+    public E pop() {
+        return list.removeFirst();
+    }
+
+    @Override
+    public E peek() {
+        return list.getFirst();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Stacke: top ");
+        res.append(list);
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack<Integer> stack = new LinkedListStack<>();
+        for (int i = 0; i < 5; i++) {
+            stack.push(i);
+            System.out.println(stack);
+        }
+        stack.pop();
+        System.out.println(stack);
+    }
+}
--- Stack/src/cn/liangyy/Main.java
+++ Stack/src/cn/liangyy/Main.java
@@ -1,18 +1,45 @@
 package cn.liangyy;
 
+import java.util.Random;
+
 /**
  * @Author: 梁歪歪 <1732178815@qq.com>
  * @Description: blog <liangyy.cn>
  * @Create 2021-04-23-10:09
  */
 public class Main {
-    public static void main(String[] args) {
-        ArrayStack<Integer> stack = new ArrayStack<>();
-        for (int i = 0; i < 5; i++) {
-            stack.push(i);
-            System.out.println(stack);
+
+    /**
+     * 测试
+     * @param stack
+     * @param opCount
+     * @return
+     */
+    private static double testStack(Stack<Integer> stack, int opCount){
+        long startTime = System.nanoTime();
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            stack.push(random.nextInt(Integer.MAX_VALUE));
         }
-        stack.pop();
-        System.out.println(stack);
+        for (int i = 0; i < opCount; i++) {
+            stack.pop();
+        }
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+
+        /**
+         * 其实LinkedListStack的时间比较复杂，因为LinkedListStack中包含更多的new操作
+         */
+        int opCount = 1000000;
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack, time: "+ time1 + "s");
+
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        double time2 = testStack(linkedListStack, opCount);
+        System.out.println("LinkedListStack, time: " + time2 + "s");
     }
 }
