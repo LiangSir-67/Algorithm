dccc8fe15cbcd476631bf9de12151741bc833a10
LiangSir-67 <1732178815@qq.com>
2021-04-26 10:35:46 +0800

    链表

---
+++ LinkedList/src/cn/liangyy/Main.java
@@ -0,0 +1,27 @@
+package cn.liangyy;
+
+/**
+ * @Author: 梁歪歪 <1732178815@qq.com>
+ * @Description: blog <liangyy.cn>
+ * @Create 2021-04-26-8:53
+ */
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+        linkedList.add(2, 666);
+        System.out.println(linkedList);
+
+        linkedList.remove(2);
+        System.out.println(linkedList);
+
+        linkedList.removeFirst();
+        System.out.println(linkedList);
+
+        linkedList.removeLast();
+        System.out.println(linkedList);
+    }
+}
