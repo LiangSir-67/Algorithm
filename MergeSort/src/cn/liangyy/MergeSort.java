6a7b2d33bda03bf926481fe01c837f4154b66a2e
LiangSir-67 <1732178815@qq.com>
2021-06-01 17:26:29 +0800

    归并排序

--- MergeSort/src/cn/liangyy/MergeSort.java
+++ MergeSort/src/cn/liangyy/MergeSort.java
@@ -13,19 +13,23 @@ public class MergeSort {
     private MergeSort(){}
 
     public static <E extends Comparable<E>> void sort(E[] arr){
-        sort(arr, 0, arr.length - 1);
+        E[] temp = Arrays.copyOf(arr, arr.length);
+        sort(arr, temp , 0, arr.length - 1);
     }
 
-    private static <E extends Comparable<E>> void sort(E[] arr, int l, int r){
+    private static <E extends Comparable<E>> void sort(E[] arr, E[] temp, int l, int r){
         if (l >= r){
             return;
         }
+
         // l + r 有可能会出现超出整型所表示的范围  这里简单优化一下，就是将加法变成减法
         //int mid = (l + r) / 2;
         int mid = l + (r - l) / 2;
-        sort(arr, l, mid);
-        sort(arr, mid + 1, r);
-        merge(arr, l, mid, r);
+        sort(arr, temp, l, mid);
+        sort(arr, temp, mid + 1, r);
+        if (arr[mid].compareTo(arr[mid + 1]) > 0){
+            merge2(arr, temp, l, mid, r);
+        }
     }
 
     /**
@@ -37,27 +41,28 @@ public class MergeSort {
      * @param r
      * @param <E>
      */
-    private static <E extends Comparable<E>> void merge(E[] arr, int l, int mid, int r){
-        E[] temp = Arrays.copyOfRange(arr, l, r+1);
+    private static <E extends Comparable<E>> void merge2(E[] arr, E[] temp, int l, int mid, int r){
+        System.arraycopy(arr, l, temp, l, r - l + 1);
 
         int i = l, j = mid + 1;
         // 每轮循环为arr[k]赋值
         for (int k = l;k <= r; k ++){
             if (i > mid){
-                arr[k] = temp[j - l];
+                arr[k] = temp[j];
                 j ++;
             }else if (j > r){
-                arr[k] = temp[i - l];
+                arr[k] = temp[i];
                 i ++;
-            }else if (temp[i - l].compareTo(temp[j - l]) <= 0){
-                arr[k] = temp[i - l];
+            }else if (temp[i].compareTo(temp[j]) <= 0){
+                arr[k] = temp[i];
                 i ++;
             }else {
-                arr[k] = temp[j - l];
+                arr[k] = temp[j];
                 j ++;
             }
         }
     }
+
 
     public static void main(String[] args) {
         int n = 100000;
