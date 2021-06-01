19fda0c22821084233974391dc223e107ac06638
LiangSir-67 <1732178815@qq.com>
2021-06-01 09:52:45 +0800

    翻转链表-非递归实现

---
+++ LinkedList/src/cn/liangyy/leetcode/Solution206.java
@@ -0,0 +1,39 @@
+package cn.liangyy.leetcode;
+
+/**
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。(非递归实现)
+ *
+ * @Author: 梁歪歪 <1732178815@qq.com>
+ * @Description: blog <liangyy.cn>
+ * @Create 2021-05-31-14:23
+ */
+public class Solution206 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
