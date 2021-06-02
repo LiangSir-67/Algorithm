bec06d231d411222029d8486ff34cdb6c8ba35e3
LiangSir-67 <1732178815@qq.com>
2021-06-02 09:38:46 +0800

    归并排序

--- MergeSort/src/cn/liangyy/MergeSort.java
+++ MergeSort/src/cn/liangyy/MergeSort.java
@@ -10,16 +10,17 @@ import java.util.Arrays;
  * @Create 2021-05-06-9:59
  */
 public class MergeSort {
-    private MergeSort(){}
-
-    // 自顶向下的归并排序
-    public static <E extends Comparable<E>> void sort(E[] arr){
-        E[] temp = Arrays.copyOf(arr, arr.length);
-        sort(arr, temp , 0, arr.length - 1);
+    private MergeSort() {
     }
 
-    private static <E extends Comparable<E>> void sort(E[] arr, E[] temp, int l, int r){
-        if (l >= r){
+    // 自顶向下的归并排序
+    public static <E extends Comparable<E>> void sort(E[] arr) {
+        E[] temp = Arrays.copyOf(arr, arr.length);
+        sort(arr, temp, 0, arr.length - 1);
+    }
+
+    private static <E extends Comparable<E>> void sort(E[] arr, E[] temp, int l, int r) {
+        if (l >= r) {
             return;
         }
 
@@ -28,25 +29,24 @@ public class MergeSort {
         int mid = l + (r - l) / 2;
         sort(arr, temp, l, mid);
         sort(arr, temp, mid + 1, r);
-        if (arr[mid].compareTo(arr[mid + 1]) > 0){
+        if (arr[mid].compareTo(arr[mid + 1]) > 0) {
             merge(arr, temp, l, mid, r);
         }
     }
 
-
     // 自底向上的归并排序
-    public static <E extends Comparable<E>> void sortBU(E[] arr){
+    public static <E extends Comparable<E>> void sortBU(E[] arr) {
         E[] temp = Arrays.copyOf(arr, arr.length);
 
         int n = arr.length;
 
         // 遍历合并的区间长度
-        for (int sz = 1; sz < n; sz += sz){
+        for (int sz = 1; sz < n; sz += sz) {
             // 遍历合并的两个区间的起始位置
             /* 合并[i,i + sz -1]和Math.min(i + sz + sz -1, n - 1) */
-            for (int i = 0; i + sz < n; i += sz + sz){
-                if (arr[i + sz - 1].compareTo(arr[i + sz]) > 0){
-                    merge(arr, temp, i, i + sz -1, Math.min(i + sz + sz -1, n - 1));
+            for (int i = 0; i + sz < n; i += sz + sz) {
+                if (arr[i + sz - 1].compareTo(arr[i + sz]) > 0) {
+                    merge(arr, temp, i, i + sz - 1, Math.min(i + sz + sz - 1, n - 1));
                 }
             }
         }
@@ -61,34 +61,33 @@ public class MergeSort {
      * @param r
      * @param <E>
      */
-    private static <E extends Comparable<E>> void merge(E[] arr, E[] temp, int l, int mid, int r){
+    private static <E extends Comparable<E>> void merge(E[] arr, E[] temp, int l, int mid, int r) {
         System.arraycopy(arr, l, temp, l, r - l + 1);
 
         int i = l, j = mid + 1;
         // 每轮循环为arr[k]赋值
-        for (int k = l;k <= r; k ++){
-            if (i > mid){
+        for (int k = l; k <= r; k++) {
+            if (i > mid) {
                 arr[k] = temp[j];
-                j ++;
-            }else if (j > r){
+                j++;
+            } else if (j > r) {
                 arr[k] = temp[i];
-                i ++;
-            }else if (temp[i].compareTo(temp[j]) <= 0){
+                i++;
+            } else if (temp[i].compareTo(temp[j]) <= 0) {
                 arr[k] = temp[i];
-                i ++;
-            }else {
+                i++;
+            } else {
                 arr[k] = temp[j];
-                j ++;
+                j++;
             }
         }
     }
 
-
     public static void main(String[] args) {
         int n = 5000000;
         Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
-        Integer[] arrBU = Arrays.copyOf(arr,arr.length);
-        SortingHelper.sortTest("MergeSort",arr);
-        SortingHelper.sortTest("MergeSortBU",arrBU);
+        Integer[] arrBU = Arrays.copyOf(arr, arr.length);
+        SortingHelper.sortTest("MergeSort", arr);
+        SortingHelper.sortTest("MergeSortBU", arrBU);
     }
 }
