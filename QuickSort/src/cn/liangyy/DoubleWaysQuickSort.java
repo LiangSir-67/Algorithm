e03c77ff5525bb164333b77d740ba1c4fed245cd
LiangSir-67 <1732178815@qq.com>
2021-06-14 22:52:21 +0800

    快速排序

--- QuickSort/src/cn/liangyy/DoubleWaysQuickSort.java
+++ QuickSort/src/cn/liangyy/DoubleWaysQuickSort.java
@@ -10,15 +10,16 @@ import java.util.Random;
  * @Create 2021-06-07-14:05
  */
 public class DoubleWaysQuickSort {
-    private DoubleWaysQuickSort(){}
-
-    public static <E extends Comparable<E>> void sort2ways(E[] arr){
-        Random random = new Random();
-        sort2ways(arr, 0, arr.length - 1,random);
+    private DoubleWaysQuickSort() {
     }
 
-    private static <E extends Comparable<E>> void sort2ways(E[] arr, int l, int r,Random random){
-        if (l >= r){
+    public static <E extends Comparable<E>> void sort2ways(E[] arr) {
+        Random random = new Random();
+        sort2ways(arr, 0, arr.length - 1, random);
+    }
+
+    private static <E extends Comparable<E>> void sort2ways(E[] arr, int l, int r, Random random) {
+        if (l >= r) {
             return;
         }
         int partition = partition(arr, l, r, random);
@@ -27,23 +28,23 @@ public class DoubleWaysQuickSort {
 
     }
 
-    private static <E extends Comparable<E>> int partition(E[] arr, int l, int r, Random random){
+    private static <E extends Comparable<E>> int partition(E[] arr, int l, int r, Random random) {
         // 生成[l,r]之间的随机索引  (random.nextInt(r - l + 1))生成的是[0,r]之间的随机索引
         int p = random.nextInt(r - l + 1) + l;
-        swap(arr, l , p);
+        swap(arr, l, p);
 
         // arr[l+1...i-1] <= v;arr[j+1...r] >= v
         int i = l + 1;
         int j = r;
-        while (true){
-            while (i <= j && arr[i].compareTo(arr[l]) < 0){
+        while (true) {
+            while (i <= j && arr[i].compareTo(arr[l]) < 0) {
                 i++;
             }
-            while (j >= i && arr[j].compareTo(arr[l]) > 0){
+            while (j >= i && arr[j].compareTo(arr[l]) > 0) {
                 j--;
             }
 
-            if (i >= j){
+            if (i >= j) {
                 break;
             }
 
